3f4ff520536be243579e9b2c695bc4f78a3fdc8d
h-x29 <huixinglee29@gmail.com>
2024-12-01 00:16:06 +0900

    Update module_A.py

--- module_A.py
+++ module_A.py
@@ -7,7 +7,9 @@ def get_subjects(file_path):
 
 def get_scores(file_path, selected_subject):
     data = pd.read_csv(file_path, encoding='euc-kr')
-    filtered_data = data[data['유형'] == selected_subject]
-    male_scores = filtered_data['남자'].dropna().tolist()  # Exclude NaN values
-    female_scores = filtered_data['여자'].dropna().tolist()  # Exclude NaN values
-    return male_scores, female_scores
+    filtered_data = data[data["유형"] == selected_subject]
+
+    scores = filtered_data['표준점수'].dropna().tolist()
+    male_count = filtered_data['남자'].dropna().tolist()  # Exclude NaN values
+    female_count = filtered_data['여자'].dropna().tolist()  # Exclude NaN values
+    return scores, male_count, female_count
