2d928005207981b5cacae04d64bf4d6e608f102a
h-x29 <huixinglee29@gmail.com>
2024-11-30 15:27:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from module_A import get_subjects
+from module_A import get_subjects, get_scores
+from module_B import draw_distribution_graph
 import sys
 sys.stdout.reconfigure(encoding='utf-8')
 
@@ -13,3 +14,7 @@ for i, subject in enumerate(subjects):
 choice = int(input("Select a subject by number: ")) - 1
 if choice < 0 or choice >= len(subjects):
     print("Invalid choice!")
+else:
+    selected_subject = subject[choice]
+    male_scores, female_scores = get_scores(file_path, selected_subject)
+    draw_distribution_graph(male_scores, female_scores, selected_subject)
