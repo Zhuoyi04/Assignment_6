9c980cf50c4059b7c3c32da3583a503956e6bae2
h-x29 <huixinglee29@gmail.com>
2024-11-30 15:27:34 +0900

    Update module_B.py

--- module_B.py
+++ module_B.py
@@ -1,3 +1,9 @@
 import matplotlib.plot as plt
 
-def draw_distribution_graph()
+def draw_distribution_graph(male_scores, female_scores, subject):
+  plt.hist([male_scores, female_scores], bins = 10, label = ['Male', 'Female'], alpha = 0.7, color = ['blue', 'pink'])
+  plt.title(f"Score Distribution for {subject}")
+  plt.xlabel("Scores")
+  plt.ylabel("Frequency")
+  plt.legend()
+  plt.show()
