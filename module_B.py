4821095ae0fbad008d533ce9fa9f839ec2f80d9c
h-x29 <huixinglee29@gmail.com>
2024-12-01 00:17:06 +0900

    Update module_B.py

--- module_B.py
+++ module_B.py
@@ -1,9 +1,27 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
 
-def draw_distribution_graph(male_scores, female_scores, subject):
-  plt.hist([male_scores, female_scores], bins = 10, label = ['Male', 'Female'], alpha = 0.7, color = ['blue', 'pink'])
+mpl.rc('font', family = 'Malgun Gothic')
+plt.rcParams['axes.unicode_minus'] = False
+
+def draw_distribution_graph(scores, male_count, female_count, subject):
+  sorted_data = sorted(zip(scores, male_count, female_count), key=lambda x: x[0])
+  scores, male_count, female_count = zip(*sorted_data)
+  
+  x = np.arange(len(scores))
+  bar_width = 0.4
+
+  plt.bar(x - bar_width / 2, male_count, bar_width, label='Male', color='blue')
+  plt.bar(x + bar_width / 2, female_count, bar_width, label='Female', color='pink')
+
+  plt.xticks(x, scores, rotation = 90, fontsize = 5)
+
   plt.title(f"Score Distribution for {subject}")
   plt.xlabel("Scores")
-  plt.ylabel("Frequency")
+  plt.ylabel("Number of Students")
   plt.legend()
+  plt.grid(axis = 'y', linestyle = '--', alpha = 0.7)
+  plt.tight_layout()
+
   plt.show()
